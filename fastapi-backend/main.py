0921b9c7d75344c76421c969673d156069fe32e3
Prahmodh R <89332177+Prahmodh-Raj1@users.noreply.github.com>
2024-01-30 21:45:48 +0530

    Updated server-side files

--- fastapi-backend/main.py
+++ fastapi-backend/main.py
@@ -37,8 +37,10 @@ except Exception as e:
     print(f"Error: {e}")
 """
 def load_ann_model():
-    # Your ann model loading code
-    return None
+    model = tf.keras.models.load_model(r'C:/Users/prahm/Downloads/sign_ann_model.hdf5', compile=False)
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+    model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
 
 def load_cnn_model():
     model = tf.keras.models.load_model(r'C:/Users/prahm/Downloads/cnn_model.hdf5', compile=False)
@@ -48,6 +50,7 @@ def load_cnn_model():
     
 
 algo_model = load_algo_model()
+ann_model = load_ann_model()
 cnn_model =load_cnn_model()
 
 @app.post("/api/upload/algo")
@@ -65,13 +68,15 @@ async def upload_file(file: UploadFile = File(...)):
     cur_img = cv2.resize(cur_img,(224,224))
     cur_img=cur_img/255
     cur_img = cur_img.reshape((1, 224, 224, 3))
-    #predcur=cnn_model.predict(forge_img)
+    cur_img = cur_img.reshape((1, -1))
+    predcur=algo_model.predict(cur_img)
     message = ""
-    a=np.argmax(algo_model.predict(cur_img), axis=1)
-    if(a==1):
-        message = "The signature is fraud"
+    
+    #a=np.argmax(algo_model.predict(cur_img), axis=1)
+    if(predcur==1):
+        message = "The signature has been forged"
     else:
-        message = "The signature is not fraud"
+        message = "The signature is real"
     return JSONResponse(content={"message": message}, status_code=200)
 
 @app.post("/api/upload/ann")
@@ -80,9 +85,25 @@ async def upload_file(file: UploadFile = File(...)):
     file.file.seek(0)  # Move the file pointer back to the beginning of the file
 
     print(f"Received file: {file.filename}, Content-Type: {file.content_type}, Size: {file_size_mb:.2f} MB")
+    pil_image = Image.open(file.file)
 
-    return JSONResponse(content={"message": "File successfully received"}, status_code=200)
-
+    # Converting the PIL image to a numpy array
+    cur_img = np.array(pil_image)
+    
+    cur_img = cv2.cvtColor(cur_img,cv2.COLOR_BGR2RGB)
+    cur_img = cv2.resize(cur_img,(224,224))
+    cur_img=cur_img/255
+    cur_img = cur_img.reshape((1, 224, 224, 3))
+    predcur=ann_model.predict(cur_img)
+    message = ""
+    a=np.argmax(ann_model.predict(cur_img), axis=1)
+    if(a==0):
+        message = "The signature is real"
+    else:
+        message = "The signature has been forged"
+    predcur=predcur*100
+    predcur = np.round(predcur,decimals=2)
+    return JSONResponse(content={"message": message,"prediction": predcur.tolist()}, status_code=200)
 @app.post("/api/upload/cnn")
 async def upload_file(file: UploadFile = File(...)):
     file_size_mb = len(file.file.read()) / (1024 * 1024)
@@ -98,14 +119,18 @@ async def upload_file(file: UploadFile = File(...)):
     cur_img = cv2.resize(cur_img,(224,224))
     cur_img=cur_img/255
     cur_img = cur_img.reshape((1, 224, 224, 3))
-    #predcur=cnn_model.predict(forge_img)
+    predcur=cnn_model.predict(cur_img)
     message = ""
     a=np.argmax(cnn_model.predict(cur_img), axis=1)
-    if(a==1):
-        message = "The signature is fraud"
+    if(a==0):
+        message = "The signature is real"
     else:
-        message = "The signature is not fraud"
-    return JSONResponse(content={"message": message}, status_code=200)
+        message = "The signature has been forged"
+    
+    predcur=predcur*100
+    
+    predcur = np.round(predcur,decimals=2)
+    return JSONResponse(content={"message": message,"prediction": predcur.tolist()}, status_code=200)
 
 
 @app.get("/")
