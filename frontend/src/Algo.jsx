4a0509628d97d768a419ccbb6544406ff1a3cf48
Prahmodh R <89332177+Prahmodh-Raj1@users.noreply.github.com>
2024-03-10 19:24:04 +0530

    Updated Algo.jsx

--- frontend/src/Algo.jsx
+++ frontend/src/Algo.jsx
@@ -56,7 +56,7 @@ function Algo() {
         <>
             <div className="h-screen flex flex-col">
                 <div className="text-center text-4xl font-bold mt-8">
-                    <StyledText text="IMPLEMENTATION USING ML ALGORITHM" color="#0FF0FC" />
+                    <StyledText text="IMPLEMENTATION USING BEST PERFORMING ML ALGORITHM" color="#0FF0FC" />
                 </div>
                 <div className="mt-16 mx-auto">
                     <div className="mb-4 text-center">
@@ -64,25 +64,27 @@ function Algo() {
                     </div>
                     <div className="flex justify-center gap-10">
                         <Button onClick={handleUpload} color="#0FF0FC" text="Upload and Test" />
-                        <Button onClick={clearFile} color="#f44c4c" text="Clear" />
+                        <Button onClick={clearFile} color="#0FF0FC" text="Clear" />
                     </div>
 
                     {error && <p className="text-red-500 text-center mt-4">{error}</p>}
                     {loading && <p className="text-center mt-4">Loading...</p>}
                     {selectedFile && (
-                        <div className="flex flex-wrap justify-center py-8">
-                            <div className="w-full sm:w-auto mb-6 mr-4">
-                                <div>
-                                    <br />
-                                    <img src={imageUrl} alt="Uploaded File" className="w-48 h-48 object-contain border-4 border-gray-900 bg-white mx-auto" />
-                                </div>
-                            </div>
-                            <div className="w-full sm:w-auto text-2xl font-bold flex flex-col items-center justify-center">
-                                <h2 className="mb-2 text-shadow underline shadow-2px-2px-4px text-center">RESULT</h2>
-                                <p>{result}</p>
+                        <div className="flex justify-center py-8">
+                        <div className="w-full sm:w-auto mb-6 mr-80"> 
+                            <div>
+                                <br />
+                                <img src={imageUrl} alt="Uploaded File" className="w-48 h-48 object-contain border-4 border-gray-900 bg-white mx-auto" />
                             </div>
                         </div>
+                        <div className="w-full sm:w-auto text-2xl font-bold flex flex-col items-center justify-center">
+                            <h2 className="mb-2 text-shadow underline shadow-2px-2px-4px text-center">RESULT</h2>
+                            <p>{result}</p>
+                        </div>
+                    </div>
+                    
                     )}
+
                 </div>
             </div>
         </>
